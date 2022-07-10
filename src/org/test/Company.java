0b773bdcd941b4a57f2136cf83cd8ebba58e9b6d
kaarthi <Kaarthikdhanushka2004@gmail.com>
2022-07-10 18:15:15 +0530

    code added

---
+++ src/org/test/Company.java
@@ -0,0 +1,15 @@
+package org.test;
+
+public class Company {
+private void ComId() {
+System.out.println("comId is 555");
+}
+private void CompName() {
+System.out.println("CompName is TCS");
+}
+public static void main(String[] args) {
+	Company c=new Company();
+	c.ComId();
+	c.CompName();
+}
+}
