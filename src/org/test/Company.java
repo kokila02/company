8018fda65e46c17f55adad461d2790d9c4343fd2
kaarthi <Kaarthikdhanushka2004@gmail.com>
2022-07-10 18:24:37 +0530

    added

--- src/org/test/Company.java
+++ src/org/test/Company.java
@@ -3,9 +3,13 @@ package org.test;
 public class Company {
 private void ComId() {
 System.out.println("comId is 555");
+
 }
 private void CompName() {
 System.out.println("CompName is TCS");
+}
+private void CompPlace() {
+System.out.println("company place is chennai");
 }
 public static void main(String[] args) {
 	Company c=new Company();
